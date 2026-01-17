2114c42dc1bebac04c259437331c4bb79f004b6d
MirThara <mi.tha.14@icloud.com>
2026-01-17 12:47:06 +0100

    added card

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -2,27 +2,27 @@ import { createPiano } from "./components/piano.js";
 import { createStaff } from "./components/staff.js";
 
 export const cards = [
-    // {
-    //     id: "interval-piano-1",
-    //     prompt: "Markiere eine große Terz",
-    //     type: "piano-interval",
+    {
+        id: "interval-piano-1",
+        prompt: "Markiere eine große Terz",
+        type: "piano-interval",
 
-    //     setup({ contentEl, setAnswer, setValid }) {
-    //         const piano = createPiano({
-    //             onChange(selection) {
-    //                 setAnswer(selection);
-    //                 setValid(selection.length === 2);
-    //             }
-    //         });
+        setup({ contentEl, setAnswer, setValid }) {
+            const piano = createPiano({
+                onChange(selection) {
+                    setAnswer(selection);
+                    setValid(selection.length === 2);
+                }
+            });
 
-    //         contentEl.appendChild(piano);
-    //     },
+            contentEl.appendChild(piano);
+        },
 
-    //     check(answer) {
-    //         if (answer.length !== 2) return false;
-    //         return Math.abs(answer[1] - answer[0] === 4);
-    //     }
-    // },
+        check(answer) {
+            if (answer.length !== 2) return false;
+            return Math.abs(answer[1] - answer[0] === 4);
+        }
+    },
     {
         id: "note-entry-1",
         prompt: "Trage g' in das Notensystem ein",
